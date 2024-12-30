9c7f722037885ce81ef7351eb2f668e7ab204205
unknown <ddddddddebat@gmail.com>
2024-12-30 13:06:34 +0300

    windows crash recovery

--- JobProject/src/main/java/com/example/JobProject/controller/UserController.java
+++ JobProject/src/main/java/com/example/JobProject/controller/UserController.java
@@ -4,15 +4,23 @@ import com.example.JobProject.dto.RoleDto;
 import com.example.JobProject.dto.UserDto;
 import com.example.JobProject.service.UserService;
 import lombok.AllArgsConstructor;
+
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/users")
@@ -39,5 +47,23 @@ public class UserController {
     @GetMapping ("/all")
     public ResponseEntity<List<UserDto>> findAll() {
         return new ResponseEntity<>(userService.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/roles")
+    public ResponseEntity<Map<String, Object>> getUserRoles(Authentication authentication) {
+        Map<String, Object> response = new HashMap<>();
+
+        // Получаем роли из Authentication объекта
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+
+        // Извлекаем роли
+        List<String> roles = authorities.stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList());
+
+        // Добавляем роли в ответ
+        response.put("roles", roles);
+
+        return ResponseEntity.ok(response);
     }
 }
